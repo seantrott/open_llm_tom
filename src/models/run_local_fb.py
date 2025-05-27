c25d8632c88e64d13bb941b97922e21275c8d9f3
Sean <trott.sean@gmail.com>
2025-05-27 09:37:03 -0700

    OLMO

--- src/models/run_local_fb.py
+++ src/models/run_local_fb.py
@@ -8,6 +8,13 @@ import torch
 from tqdm import tqdm
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
+
+MODELS = {
+    'allenai/OLMo-2-1124-7B': 'OLMo 2 7B'
+}
+
+
+
 def next_seq_prob(model, tokenizer, seen, unseen):
     device = next(model.parameters()).device  # get model's actual device
     input_ids = tokenizer.encode(seen, return_tensors="pt").to(device)
@@ -35,67 +42,77 @@ def next_seq_prob(model, tokenizer, seen, unseen):
 
 
 
-### TESTING OUT
+
+def main(model_path):
+
+    # Set up save path, filename, etc.
+    savepath = f"data/processed/fb_local/"
+    if not os.path.exists(savepath): 
+        os.makedirs(savepath)
+
+    if "/" in model_path:
+        filename = "fb-" + model_path.split("/")[1] + ".csv"
+    else:
+        filename = "fb-" + model_path + ".csv"
+
+    print(filename)
+    print(savepath)
+
+	### Load model
+	model = AutoModelForCausalLM.from_pretrained(
+	    model_path,
+	    device_map="auto"  # spread across available GPUs
+	)
+	tokenizer = AutoTokenizer.from_pretrained(model_path)
 
 
-### Load data
-df_fb = pd.read_csv("data/raw/fb.csv")
-
-### Load model
-mpath = "allenai/OLMo-2-1124-7B"
-model = AutoModelForCausalLM.from_pretrained(
-    mpath,
-    device_map="auto"  # spread across available GPUs
-)
-tokenizer = AutoTokenizer.from_pretrained(mpath)
-
-### Run model
-results = []
-
-with tqdm(total=len(df_fb)) as pbar:
-    for index, row in df_fb.iterrows():
-        passage = row['passage'].replace("[MASK].", "")
-        sep = " " if not passage.endswith(" ") else ""
-        start_location = sep + row['start']
-        end_location = sep + row['end']
+	### Load data
+	df_fb = pd.read_csv("data/raw/fb.csv")
 
 
-        start_prob = next_seq_prob(model, tokenizer, passage, start_location)
-        end_prob = next_seq_prob(model, tokenizer, passage, end_location)
-
-        if start_prob == 0 or end_prob == 0:
-            continue
-
-        results.append({
-            'start_prob': start_prob,
-            'end_prob': end_prob,
-            'passage': row['passage'],
-            'start': row['start'],
-            'end': row['end'],
-            'knowledge_cue': row['knowledge_cue'],
-            'first_mention': row['first_mention'],
-            'recent_mention': row['recent_mention'],
-            'log_odds': np.log2(start_prob / end_prob),
-            'condition': row['condition']
-        })
+	### Run model
+	with tqdm(total=len(df_fb)) as pbar:
+	    for index, row in df_fb.iterrows():
+	        passage = row['passage'].replace("[MASK].", "")
+	        sep = " " if not passage.endswith(" ") else ""
+	        start_location = sep + row['start']
+	        end_location = sep + row['end']
 
 
-        
-        pbar.update(1)
+	        start_prob = next_seq_prob(model, tokenizer, passage, start_location)
+	        end_prob = next_seq_prob(model, tokenizer, passage, end_location)
 
-df_results = pd.DataFrame(results)
+	        if start_prob == 0 or end_prob == 0:
+	            continue
 
-### Quick check
-from scipy.stats import ttest_ind
-# Separate into two groups
-group1 = df_results[df_results['condition'] == 'True Belief']['log_odds']
-group2 = df_results[df_results['condition'] == 'False Belief']['log_odds']
+	        results.append({
+	            'start_prob': start_prob,
+	            'end_prob': end_prob,
+	            'passage': row['passage'],
+	            'start': row['start'],
+	            'end': row['end'],
+	            'knowledge_cue': row['knowledge_cue'],
+	            'first_mention': row['first_mention'],
+	            'recent_mention': row['recent_mention'],
+	            'log_odds': np.log2(start_prob / end_prob),
+	            'condition': row['condition']
+	        })
 
-# Run independent t-test (assumes unequal variance by default)
-t_stat, p_val = ttest_ind(group1, group2, equal_var=False)
 
-print(f"T-statistic: {t_stat:.3f}")
-print(f"P-value: {p_val:.3f}")
+	        
+	        pbar.update(1)
 
-print(df_results.groupby("condition").mean("log_odds"))
+	### Create DataFRame
+	df_results = pd.DataFrame(results)
+    df_results['model_path'] = model_path
+    df_results['model_shorthand'] = MODELS[model_path]
+
+    df_results.to_csv(os.path.join(savepath,filename), index=False)
+
+
+if __name__ == "__main__":
+
+    model_path = "allenai/OLMo-2-1124-7B"
+    main(model_path)
+
 
