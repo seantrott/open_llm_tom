4ed7819f38bce76a20deff309e6eafb1e36777ae
Sean <trott.sean@gmail.com>
2025-06-09 08:34:14 -0700

    emma models

--- src/models/run_local_fb.py
+++ src/models/run_local_fb.py
@@ -57,8 +57,8 @@ MODELS = {
     "Qwen/Qwen2.5-32B-Instruct": "Qwen 2.5 32B Instruct",
 
     ### Gemma (needs authentication)
-    "google/gemma-2b": "Gemma 2 2B",
-    "google/gemma-2b-it": "Gemma 2 2B Instruct",
+    # "google/gemma-2b": "Gemma 2 2B",
+    # "google/gemma-2b-it": "Gemma 2 2B Instruct",
     "google/gemma-7b": "Gemma 2 7B",
     "google/gemma-7b-it": "Gemma 2 7B Instruct",
 
